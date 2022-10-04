41a1159e5737bdd228448ae0da32875b2c1439f0
chaarz <cduppre@gmail.com>
2022-10-04 17:14:23 -0400

    fetched data from auto-complete endpoint and created dropdown menu

--- script.js
+++ script.js
@@ -3,18 +3,48 @@
 // API Key (Charlotte): c03cc9a000msha3fe9d73a2d9dfdp16bddfjsn7cf0888a090e
 // API Key (Anjalee): 3d98be6ce0mshbad421f97ff1ba1p1f94acjsnc1505c33d623
 
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '3d98be6ce0mshbad421f97ff1ba1p1f94acjsnc1505c33d623',
-        'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-    }
-};
+const app = {};
 
-fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+app.apiKey = 'c03cc9a000msha3fe9d73a2d9dfdp16bddfjsn7cf0888a090e';
+
+app.getAutoComplete = function(ingredient){
+    const url = new URL('https://tasty.p.rapidapi.com/recipes/auto-complete')
+    url.search = new URLSearchParams ({
+        prefix: ingredient
+    });
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3d98be6ce0mshbad421f97ff1ba1p1f94acjsnc1505c33d623',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+    
+    fetch(url, options)
+        .then(response => response.json())
+        .then(jsonData => {
+            // console.log(jsonData) //check to see the name of the array to use in the app.displayDropdown method i.e. 'results'
+            app.displayDropDown(jsonData.results);
+            console.log(jsonData.results)
+        })
+        .catch(err => console.error(err));
+
+}
+
+app.displayDropDown = function(arrayofResults){
+    arrayofResults.forEach((result)=>{
+        const searchValue = result.search_value;
+
+        const dataList = document.querySelector('#searchList');
+
+        dataList.innerHTML = `<option value="${searchValue}">`;
+ 
+        console.log(searchValue)
+    })
+
+}
+
 
 // const options = {
 //     method: 'GET',
@@ -29,3 +59,8 @@ fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_mi
 //     .then(response => console.log(response))
 //     .catch(err => console.error(err));
 
+app.init = function () {
+    app.getAutoComplete('r');
+};
+
+app.init();
